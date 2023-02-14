77d44ff6c2822ffbbbb26a5dc9f0b4c53e51bd59
Jakub Prekwa <jakub.prekwa@gmail.com>
2023-02-14 15:52:23 +0100

    Set check in and check out dates

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -15,14 +15,33 @@ public class HotelSearch {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         driver.get("http://www.kurs-selenium.pl/demo/");
 
         //looking for hotel
         driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
         driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
-
         driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
+
+        //setting check in and check out dates using sendKeys
+        driver.findElement(By.name("checkin")).sendKeys("20/02/2023");
+        driver.findElement(By.name("checkout")).sendKeys("21/02/2023");
+
+        //setting check in and check out dates using calendar
+        driver.findElement(By.name("checkin")).click();
+        driver.findElements(By.xpath("//td[@class='day ' and text()='15']"))
+                .stream()
+                .filter(element -> element.isDisplayed())
+                .findFirst()
+                .ifPresent(element -> element.click());
+
+        driver.findElement(By.name("checkout")).click();
+        driver.findElements(By.xpath("//td[@class='day ' and text()='16']"))
+                .stream()
+                .filter(element -> element.isDisplayed())
+                .findFirst()
+                .ifPresent(element -> element.click());
+
     }
 
 }
