6a2bf291ca0d3f5b2cdb1b568179840bf9d8d954
Jakub Prekwa <jakub.prekwa@gmail.com>
2023-02-19 13:31:21 +0100

    Add config file

--- src/test/java/pl/seleniumdemo/tests/BaseTest.java
+++ src/test/java/pl/seleniumdemo/tests/BaseTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pl.seleniumdemo.utils.DriverFactory;
 
+import java.io.IOException;
 import java.time.Duration;
 
 public class BaseTest {
@@ -12,8 +13,8 @@ public class BaseTest {
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setup() {
-        driver = DriverFactory.getDriver("firefox");
+    public void setup() throws IOException {
+        driver = DriverFactory.getDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         driver.get("http://www.kurs-selenium.pl/demo/");
